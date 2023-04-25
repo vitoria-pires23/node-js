6f6468ca79fe379d5774ecbb2fc3ea15a0d8354b
vitoria pires <vitoria.pires23.03@gmai.com>
2023-04-25 11:01:52 -0300

    aquivos atividade

---
+++ modulos/fileSystem/demo_appendFile.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+}); 
+
+var fs = require('fs');
+
+fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
+  if (err) throw err;
+  console.log('Updated!');
+}); 
+
+var fs = require('fs');
+
+fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+  if (err) throw err;
+  console.log('Replaced!');
+}); 
